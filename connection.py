a58859ba835d9b0cb5a73b2a6e16c8acc710b7e5
Gabriel Lima <42157830+Gabriellimmaa@users.noreply.github.com>
2022-09-22 21:17:22 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
 class MongoDB:
     def __init__(self):
-        cluster = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://gabriellima:Y6VuDgg8QNVylIF1@cluster0.polpmg2.mongodb.net/test")
+        cluster = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
 
         main = cluster.main
         self.dbuser = main.user
